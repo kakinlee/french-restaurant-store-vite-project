c9152c19f5a8b5c16a33c6ca8b2977135689e51d
kakin lee <kakinmusic5@gmail.com>
2024-12-19 22:31:32 +0900

    # Changes to be committed: #	modified:   deploy.sh #	modified:   package-lock.json #	modified:   package.json #	modified:   vite.config.ts #

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { fileURLToPath, URL } from 'url'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/vite-project/" ,// 指定部署路徑,
   plugins: [react()],
-  build: {
-    outDir: 'dist', // 指定輸出目錄
-  },
-  base: "/",
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+     },
+    }
 })
