368c35ae5c2fd627e3f975afa76c59ace88e41da
kakin lee <kakinmusic5@gmail.com>
2024-12-20 10:49:11 +0900

    Deploy website

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/vite-project/',
+  base: './',
   build: {
     outDir: 'docs', // 輸出目錄為 docs
   },
